55396fe3f9d4e46809da9505c29e9c1febc5df71
BimalGhimire38 <bimalghimire116@gmail.com>
2025-10-15 14:08:38 +0545

    super

--- In_Spyder/8_inheritance_method_overloading.py
+++ In_Spyder/8_inheritance_method_overloading.py
@@ -1,5 +1,5 @@
 class Phone:
-    def __init(self,price,brand,camera):
+    def __init__(self,price,brand,camera):
         print("Inside Phone constructor")
         self.__price = price 
         self.brand = brand
@@ -9,9 +9,12 @@ class Phone:
         print("Buying a Phone")
         
 class SmartPhone(Phone):
-    def buy(self):
-        print("Buying a smartphone")
-        super().buy()
+    def __init__(self,price,brand,camera,os,ram):
+        super().__init__(price,brand,camera)
+        self.os = os
+        self.ram = ram
         
-s = SmartPhone(20000,"Samsung","48 MP")
-s.buy()
+s = SmartPhone(20000,"Samsung","48 MP","Android",8)
+
+print(s.os)
+print(s.brand)
