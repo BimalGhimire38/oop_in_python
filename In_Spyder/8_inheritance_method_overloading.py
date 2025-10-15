7c7d5262d670f5f02632e16f02d5ea3efb7c130a
BimalGhimire38 <bimalghimire116@gmail.com>
2025-10-15 10:46:46 +0545

    inheritance

--- In_Spyder/8_inheritance_method_overloading.py
+++ In_Spyder/8_inheritance_method_overloading.py
@@ -5,3 +5,22 @@ Created on Sun Sep 21 20:56:30 2025
 @author: BIMAL
 """
 
+# 2082/06/29  10:41 AM, @ Home, Bidhyapur
+
+class Parent:
+    def __init__(self, num):
+        self.__num = num
+        
+    def get_num(self):
+        return self.__num
+    
+class Child(Parent):
+    def show(self):
+        print("This is in child class")
+        
+son = Child(100)
+print(son.get_num())
+son.show()
+
+
+
